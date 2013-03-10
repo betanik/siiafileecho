0d8e92793f49802bcf5f57e7e66d925ec26aa4ef
Nathanael Ng <non@nonat.com>
2013-03-10 07:18:37 +0000

    Places file on a channel, currently a stdout

---
+++ siiafileecho/src/main/java/net/nat/siiafileecho/SftpTasklet.java
@@ -0,0 +1,52 @@
+package net.nat.siiafileecho;
+
+
+import java.io.File;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.support.MessageBuilder;
+
+public class SftpTasklet implements Tasklet {
+
+ private String fileName;
+ private MessageChannel sftpChannel;
+
+ @Override
+ public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+   File file = new File(fileName);
+   if (file.exists()) {
+     Message message = MessageBuilder.withPayload(file).build();
+     try {
+       sftpChannel.send(message);
+     } catch (Exception e) {
+       System.out.println("Could not send file per SFTP: " + e);
+     }
+   } else {
+     System.out.println("File does not exist.");
+   }
+   return RepeatStatus.FINISHED;
+ }
+
+ public String getFileName() {
+   return fileName;
+ }
+
+ public void setFileName(String fileName) {
+   this.fileName = fileName;
+ }
+
+ public MessageChannel getSftpChannel() {
+   return this.sftpChannel;
+ }
+
+ public void setSftpChannel(MessageChannel sftpChannel) {
+   this.sftpChannel = sftpChannel;
+ }
+}
